ec99b44e4685ec6317a64b64f15d4fd903008966
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-02-01 19:16:22 +0300

    Названия пунктов меню для заметок

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,5 +1,9 @@
-class Note (val title: String, val content: String) {
+class Note (val title: String, val content: String) : Menunable {
     override fun toString(): String {
         return "Название: \"$title\", текст: \"$content\"."
+    }
+
+    override fun menu_name(): String {
+        return title
     }
 }
