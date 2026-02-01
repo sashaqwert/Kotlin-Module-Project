feb059a58fe6f010b336d33568b2efdcdab03827
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-02-01 19:05:41 +0300

    main :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,8 @@
 import java.util.Scanner
 
 val inp = Scanner(System.`in`)
-val archives: ArrayList<Archive> = ArrayList()
 
-fun main(args: Array<String>) {
-    menu_archive_list()
-}
-
-fun menu_archive_list() {
-    println("Список архивов:")
-    println("0. Создать архив")
-    if (archives.isEmpty()) {
-        println("Список пуст")
-    }
-    else {
-        for (i in 1..archives.size) {
-            println("${i}. ${archives.get(i - 1).name}")
-        }
-    }
-    println("${archives.size + 1}. Выход")
-
+fun main() {
+    val archiveMenu = ArchiveMenu()
+    archiveMenu.start()
 }
