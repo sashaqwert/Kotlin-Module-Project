ca4341c4fc79121784f7dee4ef6b138272409f44
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2026-02-01 16:46:14 +0300

    Main (WIP)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,34 @@
+import java.util.Scanner
+
+val inp = Scanner(System.`in`)
+val archives: ArrayList<Archive> = ArrayList()
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    menu_archive_list()
+}
+
+fun menu_archive_list() {
+    println("Список архивов:")
+    println("0. Создать архив")
+    if (archives.isEmpty()) {
+        println("Список пуст")
+    }
+    else {
+        for (i in 1..archives.size) {
+            println("${i}. ${archives.get(i - 1).name}")
+        }
+    }
+    println("${archives.size + 1}. Выход")
+
+}
+
+fun create_archive() {
+    println("Введите название архива. Для отмены оставьте строку пустой.")
+    val name = inp.nextLine()
+    if (name != "") {
+        archives.add(Archive(name))
+        println("Архив создан.")
+    } else {
+        println("Создание архива отменено")
+    }
 }
